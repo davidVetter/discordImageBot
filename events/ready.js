aac61e3dd3141e9a1c5b9abe13a005da77f082c8
David Vetter <ddvetter23@gmail.com>
2023-01-13 21:12:26 -0600

    moved ready event to module

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Ready! Logged in as this person >> ${client.user.tag}`);
+  },
+};
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -48,11 +48,11 @@ for (const file of commandFiles) {
 //   }
 // });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// // When the client is ready, run this code (only once)
+// // We use 'c' for the event parameter to keep it separate from the already defined 'client'
+// client.once(Events.ClientReady, (c) => {
+//   console.log(`Ready! Logged in as ${c.user.tag}`);
+// });
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
