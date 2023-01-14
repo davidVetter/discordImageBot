7212937083fbc4a9586be2476f4cf75d0685d0b7
David Vetter <ddvetter23@gmail.com>
2023-01-14 03:18:15 -0600

    added tmp directory to gitignore, setup defer reply for imagine

--- commands/imagine.js
+++ commands/imagine.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
+const { generateImage } = require("../src/generateImage");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,6 +13,23 @@ module.exports = {
     .setDescription("Imagine an IMAGE!"),
   async execute(interaction) {
     console.log(interaction.options.getString('prompt'));
-    await interaction.reply({ content: "Pong to Pong Pong Pong!" });
+    await interaction.deferReply();
+
+    const { file } = await generateImage(
+      interaction.id,
+      interaction.options.getString('prompt')
+    );
+
+    await interaction.editReply({
+      embeds: [
+        {
+          title: interaction.options.getString('prompt'),
+          image: {
+            url: 'attachment://image.jpg',
+          },
+        },
+      ],
+      files: [new AttachmentBuilder(file, "image.jpg")],
+    });
   },
 };
