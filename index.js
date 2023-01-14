b5b2e46f26a67da92e6bf9f57cc14db58320f1a2
David Vetter <ddvetter23@gmail.com>
2023-01-13 21:08:53 -0600

    moved interactionCreate event to module

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, "commands");
+const commandPath = path.join(__dirname, "commands");
 const commandFiles = fs
-  .readdirSync(commandsPath)
+  .readdirSync(commandPath)
   .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandPaths, file);
+  const filePath = path.join(commandPath, file);
   const command = require(filePath);
   // Set a new item in the Collection with the key as the command name and the value as the exported module
   if ("data" in command && "execute" in command) {
@@ -27,26 +27,26 @@ for (const file of commandFiles) {
   }
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  console.log(interaction);
-  const command = interaction.client.commands.get(interaction.commandName);
+// client.on(Events.InteractionCreate, async (interaction) => {
+//   if (!interaction.isChatInputCommand()) return;
+// //   console.log(interaction);
+//   const command = interaction.client.commands.get(interaction.commandName);
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
+//   if (!command) {
+//     console.error(`No command matching ${interaction.commandName} was found.`);
+//     return;
+//   }
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
+//   try {
+//     await command.execute(interaction);
+//   } catch (error) {
+//     console.error(error);
+//     await interaction.reply({
+//       content: "There was an error while executing this command!",
+//       ephemeral: true,
+//     });
+//   }
+// });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
