3414d0270a5da00ecead30921b149bc5a65e41f0
David Vetter <ddvetter23@gmail.com>
2023-01-13 20:08:52 -0600

    added event listener for commands

--- index.js
+++ index.js
@@ -27,9 +27,25 @@ for (const file of commandFiles) {
   }
 }
 
-client.on(Events.InteractionCreate, (interaction) => {
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
   console.log(interaction);
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 // When the client is ready, run this code (only once)
