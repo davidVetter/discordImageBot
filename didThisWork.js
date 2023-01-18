c1e379e2d8a508b1057ed1b8101c82a96644a0c6
David Vetter <ddvetter23@gmail.com>
2023-01-18 12:29:03 -0600

    removed old code

--- didThisWork.js
+++ didThisWork.js
@@ -1,16 +1,13 @@
 const { spawn } = require("node:child_process");
 const fs = require("node:fs");
-const { copyFile } = require('node:fs/promises');
 
 if (!fs.existsSync("./tmp")) {
   fs.mkdirSync("./tmp");
 }
 
 async function generateImage(id, prompt) {
-//   const outputDir = `output/${id}`;
 
   await new Promise((resolve) => {
-    // const options = ["--model", "SD-2.1", "--outdir", outputDir, prompt];
     fs.writeFileSync('.\\testPrompt.txt', prompt);
 
     const cmd = spawn('cmd.exe', ['/c', 'invoke.bat']);
@@ -33,12 +30,7 @@ async function generateImage(id, prompt) {
     .readdirSync(`C:\\Users\\David\\InvokeAI\\outputs`)
     .filter((file) => file.endsWith(".png"));
 
-    // console.log('This is jpegs first: ', jpegs[0], ' Jpegs last: ', jpegs[jpegs.length-1] );
-
     const file = `C:\\Users\\David\\InvokeAI\\outputs\\${jpegs[jpegs.length-1]}`;
-    // const file = 'C:\\Users\\David\\InvokeAI\\outputs\\testOut.png';
-
-    // fs.rmSync(`./${outputDir}`, { recursive: true });
 
     return file;
 }
