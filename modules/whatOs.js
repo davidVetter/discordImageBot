bb19da88e5f063324e06f1423cbea8331547af4f
David Vetter <ddvetter23@gmail.com>
2023-01-19 15:25:34 -0600

    consistent file names for generateImage files, updated whatOS module to new file names

--- modules/whatOs.js
+++ modules/whatOs.js
@@ -2,11 +2,11 @@ function whatOs() {
     const os = process.platform;
     let generateImageFile;
     if (os === 'darwin') {
-      generateImageFile = '../src/generateImage.js';
+      generateImageFile = '../src/macGenerateImage.js';
     } else if (os === 'linux') {
       generateImageFile = '../src/linuxGenerateImage.js';
     } else if ( os === 'win32') {
-      generateImageFile = '..\\src\\generateImageWindows.js';
+      generateImageFile = '..\\src\\windowsGenerateImage.js';
     }
   
     return generateImageFile;
