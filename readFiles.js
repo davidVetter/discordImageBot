650b6244a3522f76bac260511aa3d010b7dfc0f0
David Vetter <ddvetter23@gmail.com>
2023-01-19 12:42:48 -0600

    testing dynamic file imports for generating images"

--- readFiles.js
+++ readFiles.js
@@ -1,27 +1,59 @@
-const fs = require("node:fs");
-const { copyFile } = require("node:fs/promises");
+// const fs = require("node:fs");
+// const { copyFile } = require("node:fs/promises");
 
-const jpegs = fs
-  .readdirSync(`C:\\Users\\David\\InvokeAI\\outputs`)
-  .filter((file) => file.endsWith(".png"));
+// const jpegs = fs
+//   .readdirSync(`C:\\Users\\David\\InvokeAI\\outputs`)
+//   .filter((file) => file.endsWith(".png"));
 
-console.log(
-  "This is jpegs first: ",
-  jpegs[0],
-  " Jpegs last: ",
-  jpegs[jpegs.length - 1]
-);
+// console.log(
+//   "This is jpegs first: ",
+//   jpegs[0],
+//   " Jpegs last: ",
+//   jpegs[jpegs.length - 1]
+// );
 
-const file = `C:\\Users\\David\\codePlayground\\discordImageBot\\tmp\\test.png`;
+// const file = `C:\\Users\\David\\codePlayground\\discordImageBot\\tmp\\test.png`;
 
-console.log("This is file", file);
+// console.log("This is file", file);
 
-try {
-  copyFile(
-    `C:\\Users\\David\\InvokeAI\\outputs\\${jpegs[jpegs.length - 1]}`,
-    file
-  );
-} catch {
-  console.error("The file could not be copied");
+// try {
+//   copyFile(
+//     `C:\\Users\\David\\InvokeAI\\outputs\\${jpegs[jpegs.length - 1]}`,
+//     file
+//   );
+// } catch {
+//   console.error("The file could not be copied");
+// }
+
+// function osfunction ()
+// {
+//     let os = process.platform;
+    
+//     console.log(' This is the OS', os);
+
+// }
+
+// osfunction();
+
+
+function whatOs() {
+  const os = process.platform;
+  let generateImageFile;
+  console.log(' This is the OS', os);
+  if (os === 'darwin') {
+    generateImageFile = '/Users/Dvetter/Documents/codePlayground/discordImageBot/src/generateImage.js';
+  } else if (os === 'linux') {
+    generateImageFile = '../src/linuxGenerateImage';
+  } else if ( os === 'win32') {
+    generateImageFile = '../src/generateImageWindows';
+  }
+  console.log('this is generateImageFile: ', generateImageFile);
+  return generateImageFile;
 }
+const generateImageFile = whatOs();
+
+const { generateImage } = require(generateImageFile);
+
+generateImage('28', 'rocket ship (high quality) (high resolution) (digital art)');
+
 
